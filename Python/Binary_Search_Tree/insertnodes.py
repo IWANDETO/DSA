53e4e79e4bbf4077644706cb9daed976318c0af5
IWANDETO <wandettoian@gmail.com>
2024-04-24 16:41:27 +0300

    Checked comments

--- Python/Binary_Search_Tree/insertnodes.py
+++ Python/Binary_Search_Tree/insertnodes.py
@@ -3,7 +3,7 @@
 # This program illustrates Inserting Nodes in a Binary Search Tree
 
 class Node():
-    def __init__(self, val): # Method to create and initialise nodes
+    def __init__(self, val): # Constructor to create and initialise nodes
         self.key = val
         self.left = None
         self.right = None
@@ -16,17 +16,17 @@ def insert(root, val): # Function used to insert nodes into the BST
     elif root.key > val: # Check if val is less than root and insert it to the left side of root
         root.left = insert(root.left, val)
             
-    return root
+    return root # Return root value to the calling function
 
 
 if __name__ == "__main__":
     root = None
 
     print("inserting element:", 100)
-    root = insert(root, 100)
+    root = insert(root, 100) # Insert root element using the Insert function
 
     print("inserting element:", 50)
-    root = insert(root, 50)
+    root = insert(root, 50) # Insert elements using the Insert function
 
     print("inserting element:", 150)
     root = insert(root, 150)
